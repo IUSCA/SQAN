a0472eb5d329c3d6797fd3c13a0306c3bb29574f
Soichi Hayashi <hayashis@soichi7.ppa.iu.edu>
2015-11-17 22:14:22 +0000

    started working on footer

--- ui/js/controllers.js
+++ ui/js/controllers.js
@@ -1,10 +1,5 @@
 'use strict';
 
-/*
- * Right now, we are going to have a single module for our app which contains
- * all controllers. In the future, we should refactor into multiple modules. When I do, don't forget
- * to add it to app.js's module list
- * */
 app.controller('HeaderController', ['$scope', 'appconf', '$route', 'toaster', '$http', 'jwtHelper', 'serverconf', 'menu',
 function($scope, appconf, $route, toaster, $http, jwtHelper, serverconf, menu) {
     $scope.title = appconf.title;
@@ -46,18 +41,9 @@ function($scope, appconf, toaster, $http, jwtHelper, $location, serverconf, scaM
         res.data.iibisids.forEach(function(research) {
             iibisids[research._id] = research;
         });
-        
-        /*
-        var serieses = {};
-        res.data.serieses.forEach(function(series) {
-            serieses[series._id] = series;
-            //series.research = iibisids[series.research_id];
-        });
-        */
-
+       
         //serises catalog (organized under Modality)
         var serieses = res.data.serieses;
-        //console.dir($scope.serieses);
 
         //organize study under iibisid / modality / subject / series / study(#series_number)
         $scope.iibisids = {};
@@ -189,6 +175,11 @@ function($scope, appconf, toaster, $http, jwtHelper, $location, serverconf, scaM
             if(!$scope.content_affix) $scope.$apply(function() {
                 $scope.content_affix = true;
             });
+
+            if(window.scrollY > document.body.clientHeight - window.innerHeight - 200) {
+                //affix, but I need to raise the bottom
+                console.log("TODO - raise bottom!");
+            }
         }
         
         //find first subject displayed
@@ -216,132 +207,6 @@ function($scope, appconf, toaster, $http, jwtHelper, $location, serverconf, scaM
         $anchorScroll(id);
     }
 }]);
-
-/*
-app.controller('RecentOldController', ['$scope', 'appconf', 'toaster', '$http', 'jwtHelper', '$location', 'menu', 'serverconf',
-function($scope, appconf, toaster, $http, jwtHelper, $location, menu, serverconf) {
-    menu.then(function(_menu) { $scope.menu = _menu; });
-    serverconf.then(function(_serverconf) { $scope.serverconf = _serverconf; });
-    $scope.appconf = appconf;
-
-    $http.get(appconf.api+'/study/recent')
-    .then(function(res) {
-
-        var iibisids = {};
-        res.data.iibisids.forEach(function(research) {
-            iibisids[research._id] = research;
-        });
-        
-        var serieses = {};
-        res.data.serieses.forEach(function(series) {
-            serieses[series._id] = series;
-        });
-
-        $scope.iibisids = {};
-        $scope.study_count = 0;
-        
-        //add series/research object references
-        res.data.studies.forEach(function(study) {
-            $scope.study_count++;
-            study.series = serieses[study.series_id];
-            
-            //organize each study under research / series
-            var research = $scope.iibisids[study.series.research_id];
-            if(research === undefined) {
-                research = {
-                    research: iibisids[study.series.research_id],
-                    serieses: {}
-                };
-                $scope.iibisids[study.series.research_id] = research;
-            }
-            var series = research.serieses[study.series_id];
-            if(series === undefined) {
-                series = {
-                    series: serieses[study.series_id],
-                    templates: {},
-                    studies: {}
-                }
-                research.serieses[study.series_id] = series;
-            }
-            series.studies[study._id] = study;
-
-            //find templates for this series
-            res.data.templates.forEach(function(template) {
-                if(template.series_id == study.series_id) series.templates[template._id] = template;
-            });
-        });
-    });
-    
-    $scope.load_study = function(study) {
-        //load qc results for each study (just queue request up and let browser take care of it)
-        //study.loading = true;
-        if(study.images) return; //already loaded
-        $http.get(appconf.api+'/study/qc/'+study._id)
-        .then(function(res) {
-            //study.loading = false;
-            study.images = res.data;
-            study.images.forEach(computeColor);
-        });
-    }
-
-    $scope.load_image = function(study, series, image) {
-        study.active_image = image;
-        study.loading = true;
-        $http.get(appconf.api+'/image/'+image._id)
-        .then(function(res) {
-            study.loading = false;
-            study.active_tab = "issues";
-            for(var k in res.data) {
-                study.active_image[k] = res.data[k];
-            }
-
-            if(res.data.qc) {
-                //load template if qc-ed
-                //TODO - template might be missing even if it's qc-ed?
-                var template = series.templates[res.data.qc.template_id];
-                if(template)  {
-                    $http.get(appconf.api+'/template/'+template._id+'/'+image.headers.InstanceNumber)
-                    .then(function(res) {
-                        //console.dir(res.data);
-                        study.active_image.template = res.data;
-                    });
-                }
-            }
-        });
-    }
-
-    function computeColor(image) {
-        var h = 0; 
-        var s = "0%"; //saturation (default to gray)
-        var l = "50%"; //light
-        if(image.qc) {
-            if(image.qc.e > 0) {
-                h = 0; //red
-                var _s = 50-image.qc.e;
-                if(_s < 0) _l = 0;
-                s = _s+"%";
-            } else if(image.qc.w > 0) {
-                h = 60; //yellow
-                var _s = 50-image.qc.w;
-                if(_s < 0) _l = 0;
-                s = _s+"%";
-            } else {
-                h = 120; //green
-                s = "50%";
-            }
-        }
-        image.color = "hsl("+h+","+s+","+l+")";
-    }
-
-    $scope.invalidate_qc = function(study) {
-        $http.put(appconf.api+'/study/qc/invalidate/'+study._id)
-        .then(function(res) {
-            toaster.success("Invalidated");
-            //TODO reload study
-        });
-    }
-}]);
-*/
 
 app.controller('StudyController', ['$scope', 'appconf', 'toaster', '$http', 'jwtHelper', '$location', 'serverconf', '$routeParams', 'scaMessage',
 function($scope, appconf, toaster, $http, jwtHelper,  $location, serverconf, $routeParams, scaMessage) {
@@ -430,7 +295,9 @@ function($scope, appconf, toaster, $http, jwtHelper,  $location, serverconf, $ro
     $scope.showallheaders = function() {
         $scope.show_all_headers = true;
     }
-
+    $scope.opentemplate = function(id) {
+        $location.path("/template/"+id);
+    }
 }]);
 
 app.controller('TemplateController', ['$scope', 'appconf', 'toaster', '$http', 'jwtHelper', '$location', 'serverconf', '$routeParams', 'scaMessage',
@@ -447,16 +314,14 @@ function($scope, appconf, toaster, $http, jwtHelper, $location, serverconf, $rou
 
     $http.get(appconf.api+'/template/head/'+$routeParams.templateid)
     .then(function(res) {
-        $scope.head = res.data;
-        //if($scope.data.images) $scope.data.images.forEach(computeColor);
+        $scope.data = res.data;
     });
 
-    $scope.active_template = null;
     $scope.load_template = function(template) {
         $scope.active_template = template;
         $http.get(appconf.api+'/template/inst/'+template._id)
         .then(function(res) {
-            $scope.inst = res.data;    
+            $scope.image_detail = res.data;
         });
     }
 
