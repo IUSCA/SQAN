0b30a94f2acd9bcc9f2f3f0dbea9bc896b62285d
Soichi Hayashi <hayashis@iu.edu>
2016-01-13 04:00:35 +0000

    added a style to study note

--- dev.sh
+++ dev.sh
@@ -1,5 +1,5 @@
 #DEBUG=qc:* PORT=12048 nohup nodemon -i bin -i barn -i test -i ui ./www > nohup_server.out &
 
 pm2 delete dicom
-pm2 start api/dicom.js --watch --ignore-watch="\.log$ .sh$"
+pm2 start api/dicom.js --watch --ignore-watch="\.log$ \.css$ \.less$ .sh$"
 pm2 save
