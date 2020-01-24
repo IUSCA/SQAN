a12ee710cae6a9bd80703ec0a47a57f3ffb44cb5
Michael Young <youngmd@iu.edu>
2020-01-24 16:35:27 +0000

    Switched router to history mode

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -4,26 +4,26 @@ import Signin from '../views/Signin.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-    {
-        path: '/',
-        name: 'signin',
-        component: Signin
-    },
-    {
-        path: '/exams',
-        name: 'exams',
-        meta: {
-            requiresAuth: true,
-            is_admin: true
-        },
-        component: () => import(/* webpackChunkName: "exams" */ '../views/Exams.vue')
-    }
-]
-
 const router = new VueRouter({
-    routes
-})
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'signin',
+            component: Signin
+        },
+        {
+            path: '/exams',
+            name: 'exams',
+            meta: {
+                requiresAuth: true,
+                is_admin: true
+            },
+            component: () => import(/* webpackChunkName: "exams" */ '../views/Exams.vue')
+        }
+    ]
+});
+
 
 router.beforeEach((to, from, next) => {
     if (to.matched.some(record => record.meta.requiresAuth)) {
--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
     chainWebpack: config => {
         config.module
             .rule('vue')
