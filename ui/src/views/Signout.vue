66af5665258855c90a1ffa1e2b6ae5eaea2aa7d5
Charles Brandt <ccbrandt@iu.edu>
2020-05-13 15:49:41 -0400

    Making logout call to vuex store to remove JWT (#156)

--- ui/src/views/Signout.vue
+++ ui/src/views/Signout.vue
@@ -1,14 +1,6 @@
 <template>
-  <v-container
-    class="fill-height"
-    id="signout"
-    fluid
-  >
-    <v-card
-      class="mx-auto my-5 pa-4 text-center elevation-8"
-      outlined
-
-    >
+  <v-container class="fill-height" id="signout" fluid>
+    <v-card class="mx-auto my-5 pa-4 text-center elevation-8" outlined>
       <div class="display-2 ma-3">Logging you out</div>
 
       <v-progress-circular
@@ -19,14 +11,20 @@
       ></v-progress-circular>
     </v-card>
   </v-container>
-
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
   name: "signout",
+  methods: {
+    ...mapActions(["logout"])
+  },
   mounted() {
     let self = this;
+    self.logout();
+
     setTimeout(() => {
       self.$router.push({ path: "/" });
     }, 800);
@@ -35,12 +33,12 @@ export default {
 </script>
 
 <style>
-  #signout {
-    background-color: #5cadde;
-    height: 100vh;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    position: relative;
-  }
+#signout {
+  background-color: #5cadde;
+  height: 100vh;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
 </style>
