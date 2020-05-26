c3644349bf0bb2a2b0b065f05c98620f04171e42
Michael Young <youngmd@iu.edu>
2020-05-26 18:59:03 +0000

    stashing

---
+++ ui/src/components/Clipboard.vue
@@ -0,0 +1,37 @@
+<template>
+  <span ref="container">
+    <v-btn x-small @click="doCopy" color="blue lighten-3"><v-icon x-small class="mr-1">mdi-link-variant</v-icon> Copy Direct Link</v-btn>
+  </span>
+</template>
+
+<script>
+  export default {
+    name: 'Clipboard',
+    props: {
+      message: String
+    },
+    computed: {
+      msg_text() {
+        return this.message;
+      }
+    },
+    methods: {
+      doCopy: function () {
+
+        let self = this;
+        let container = this.$refs.container;
+        this.$copyText(this.msg_text, container).then(function (e) {
+          alert('Copied')
+          console.log(self.msg_text);
+          console.log(e)
+        }, function (e) {
+          alert('Can not copy')
+          console.log(e)
+        })
+      }
+    },
+    mounted() {
+      console.log(this.message);
+    }
+  }
+</script>
