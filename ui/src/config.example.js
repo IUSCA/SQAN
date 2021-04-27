2972a7554cebd8e5f949b4b6bc8cbb92c50ec6a8
Michael Young <youngmd@iu.edu>
2021-04-27 15:44:02 +0000

    Updated config example with IU Login url

--- ui/src/config.example.js
+++ ui/src/config.example.js
@@ -1,7 +1,7 @@
 const exports = {
   mode: 'demo',
   api: 'https://sqan.example.com/api/qc',
-  iucas_url: 'https://cas.iu.edu/cas/login',
+  iucas_url: 'https://idp.login.iu.edu/idp/profile/cas/login',
   cas_return: 'http://sqan.example.com/signin',
   default_redirect: '/signin',
   upload_enabled: true,
